0cd80104a1d3bbfa8c26ddfd907af2180947cbdb
Fudji <milhibisidek@gmail.com>
2015-03-15 21:08:22 +0200

    Стартовая версия

---
+++ layout/footer.php
@@ -0,0 +1,81 @@
+<section class="footer">
+    <div class="container">
+        <div class="col-lg-12 col-md-12 col-sm-12 padding-lr">
+            <div class="row">
+
+
+                <!--./col-lg-3-->
+
+
+
+                <div class="col-lg-2 col-md-2 col-sm-4">
+                    <div class="footer_div">
+                        <div class="footer_heading">
+                            <h5>Our Company</h5>
+                        </div>
+                        <!--./footer_heading-->
+                        <div class="recent_tweet">
+                            <ul>
+                                <!-- <li> <a href="#">Our Fleet </a></li> -->
+                                <li> <a href="welcome/contactUs">Contact Us</a></li>
+                                <li><a href="welcome/faqs">FAQ's</a></li>
+                                <li><a href="welcome/testimonials"> Testimonials</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!--./footer_div--> 
+                </div>
+
+
+                <!--./col-lg-3-->
+                <div class="col-lg-2 col-md-2 col-sm-4">
+                    <div class="footer_div">
+
+                        <div class="footer_heading"> 
+                            <h5>Pages</h5>
+                        </div>
+                        <!--./footer_heading-->
+                        <div class="recent_tweet">
+                            <ul>
+
+                                <li><a href="page/index/41/User Features">User Features</a></li>
+
+                                <li><a href="page/index/40/Executive Features">Executive Features</a></li>
+
+                                <li><a href="page/index/38/System Features">System Features</a></li>
+
+
+                            </ul>
+                        </div>
+                    </div>
+                    <!--./footer_div--> 
+                </div>
+
+
+
+                <div class="col-md-2">
+
+                    <p class="credit-cards"><label>Cards We Accept</label><img alt="Cards we accept" src="/assets/system_design/images/cards1.gif"></p>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+    <!--./container--> 
+</section>
+<section class="bottom_footer">
+    <div class="container">
+        <div class="col-md-8 padding-lr">
+            <div class="copyright-left">
+                <p>� Rental Car system 2015. All rights reserved.</p>
+            </div>
+        </div>
+        <div class="col-md-4 padding-lr">
+            <div class="copyright-left right-foo">
+
+                <p><a href="vk.com/milhibisidek" target="_blank"> Powered by Milhibisidek</a></p>
+            </div>
+        </div>
+    </div>
+</section>
